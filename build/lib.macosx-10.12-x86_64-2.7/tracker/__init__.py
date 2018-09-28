690997c8cf0754a9407eb11a4c5e3fb63965556e
Takuya Kato <ktakuya@Takuyas-MacBook-Pro.local>
2018-09-28 23:16:06 +0900

    Remove build/

--- build/lib.macosx-10.12-x86_64-2.7/tracker/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from tracker.tracker_wrap import PyTracker
-from tracker.tracker_wrap import py_lpf_det
-from tracker.tracker_wrap import py_lpf_ais
